d96247bcfce98885aef619678cd4138599268e15
Aleesha <aleeshay@maveric-systems.com>
2023-01-13 21:01:00 +0530

    Added DTO converter and exception

--- src/main/java/com/maveric/userservice/controller/UserController.java
+++ src/main/java/com/maveric/userservice/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.maveric.userservice.controller;
 
-import com.maveric.userservice.model.User;
+import com.maveric.userservice.dto.UserDto;
 import com.maveric.userservice.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1")
 @RestController
+@RequestMapping("/api/v1")
 public class UserController {
+
     private UserService userService;
 
     public UserController(UserService userService) {
@@ -19,8 +20,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("users/getUserByEmail/{emailId}")
-    public ResponseEntity<User> getUserByEmail(@PathVariable("emailId") String email) {
-        return new ResponseEntity<User>(userService.getUserByEmail(email), HttpStatus.OK);
+    @GetMapping("/users/getUserByEmail/{emailId}")
+    public ResponseEntity<UserDto> getUserByEmail(@PathVariable("emailId") String email){
+        return new ResponseEntity<UserDto>(userService.getUserByEmail(email), HttpStatus.OK);
     }
 }
--- src/main/java/com/maveric/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/maveric/userservice/service/impl/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package com.maveric.userservice.service.impl;
 
+import com.maveric.userservice.converter.DtoToModelConverter;
+import com.maveric.userservice.dto.UserDto;
+import com.maveric.userservice.exception.UserNotFoundException;
 import com.maveric.userservice.model.User;
 import com.maveric.userservice.repository.UserRepository;
 import com.maveric.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -14,15 +15,19 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
     @Autowired
+    private DtoToModelConverter dtoToModelConverter;
+
+    @Autowired
     public UserServiceImpl(UserRepository userRepository) {
-       super();
+        super();
         this.userRepository = userRepository;
     }
 
     @Override
-    public User getUserByEmail(String email) {
-        Optional<User> user = userRepository.findByEmail(email);
-        return user.get();
+    public UserDto getUserByEmail(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                ()-> new UserNotFoundException("User not found with email " + email));
+        return dtoToModelConverter.userToDtoUpdate(user);
     }
 }
 
